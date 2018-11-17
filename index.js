53a115ddf4f9c3928a5dd232faef7ab2a3bf3095
Hans Christian Reinl <info@drublic.de>
2018-11-17 17:52:41 +0100

    Export react-router components correctly

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import {
+  Link as _Link,
+  NavLink as _NavLink,
+  Redirect as _Redirect,
+  matchPath as _matchPath,
+  withRouter as _withRouter,
+} from 'react-router-dom'
+
 import Walls from './Walls'
 
-export {
-  Link,
-  NavLink,
-  Redirect,
-  matchPath,
-  withRouter,
-} from 'react-router-dom'
+export const Link = _Link
+export const NavLink = _NavLink
+export const Redirect = _Redirect
+export const matchPath = _matchPath
+export const withRouter = _withRouter
 
 export default Walls
