5c57d8833f0b6f44ad770ca3dd217d0bf6e419ca
Hans Christian Reinl <info@drublic.de>
2018-12-02 12:47:02 +0100

    Fix component export in typings

--- index.d.ts
+++ index.d.ts
@@ -9,17 +9,17 @@ import {
   RedirectProps,
 } from "react-router-dom";
 
-declare const Walls: React.ComponentClass<any>
+declare const Walls: React.ComponentType<any>
 
-export const Link: React.ComponentClass<LinkProps, any>
-export const NavLink: React.ComponentClass<NavLinkProps, any>
-export const Redirect: React.ComponentClass<RedirectProps, any>
+export const Link: React.ComponentType<LinkProps>
+export const NavLink: React.ComponentType<NavLinkProps>
+export const Redirect: React.ComponentType<RedirectProps>
 export const matchPath: <Params extends { [K in keyof Params]?: string }>(
   pathname: string,
   props: string | RouteProps,
   parent?: match<Params> | null
 ) => match<Params> | null;
 export const withRouter: (component: React.ComponentType<any>) =>
-  React.ComponentClass<any>;
+  React.ComponentType<any>;
 
 export default Walls
